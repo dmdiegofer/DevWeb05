d3a777980325a6c9c8bd5ccd846282ff3414e89c
dmdiegofer <69552519+dmdiegofer@users.noreply.github.com>
2024-08-31 21:58:42 -0500

    Variable concept and sintax first view

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,24 @@
+// VARIABLES
+
+var product = "Headset Gamer"; /* Variable initialized & Value assigned */
+var stock; /* variable initialized but with out value. */
+// JS A DINAMIC LANGUAGE 
+//product = true;  JS can reassign the value and the nature of a variable dinamiclly
+
+stock = true;/*  you don't have to use ';' because it doesn't return an error and the sentence works.
+                'var 1stock ;' - You can't initialize a variable starting with a number.
+                'var -stock ;' - You can't initialize a variable starting with a special character.
+                'var stock-example ;' You can't initialize a variable with a middle dash but you cant with an underscore.
+                'var stock_example ;' You can initialize like this.
+            */ 
+var prod_1 = 'Laptop',
+    stock_ = true,
+    category = 'Electronics';  // to initialize many variables at the same time.
+
+// VARIABLE STYLES
+var first_name = 'Edilberth'; // Underscore
+var secondName = 'Tulio'; // Camelcase
+var LastName = 'Aristizabal'; // Pascal Case
+var emailadress = 'edituari@wtf.com'; // Lowercase
+
+console.log(prod_1);
