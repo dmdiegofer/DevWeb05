06a6ce1dfa719f229c0bc4a510b935cea8b93888
dmdiegofer <69552519+dmdiegofer@users.noreply.github.com>
2024-09-01 22:16:12 -0500

    Diferense between var, let and const.
    The difference between var and let is that var allows you to use it globally. Instead let only allows it to be used in a block of code as a function.

---
+++ IntroduccionJS/js/type_02.js
@@ -0,0 +1,26 @@
+/* VARIABLES WITH LET */
+
+let product = 'Headset Gamer'; /* Variable initialized & Value assigned */
+let stock; /* variable initialized but with out value. */
+/* JS A DINAMIC LANGUAGE */
+/* product = true;  JS can reassign the value and the nature of a variable dinamiclly */
+/* 
+    The difference between var and let is that var allows you to use it globally.
+    Instead let only allows it to be used in a block of code as a function. 
+*/
+stock = true;
+        /*  you don't have to use ';' because it doesn't return an error and the sentence works.
+            'var 1stock ;' - You can't initialize a variable starting with a number.
+            'var -stock ;' - You can't initialize a variable starting with a special character.
+            'var stock-example ;' You can't initialize a variable with a middle dash but you cant with an underscore.
+            'var stock_example ;' You can initialize like this.
+        */ 
+let prod_1 = 'Laptop',
+    _stock = true,
+    category = 'Electronics';  /* to initialize many variables at the same time. */
+
+/* VARIABLE STYLES */
+let first_name = 'Edilberth';           /* Underscore */
+let secondName = 'Tulio';               /* Camelcase */
+let LastName = 'Aristizabal';           /* Pascal Case */
+let emailadress = 'edituari@wtf.com';   /* Lowercase */
---
+++ IntroduccionJS/js/type_04.js
@@ -0,0 +1,24 @@
+/* VARIABLES WITH CONST 
+    is not really a variable, actually is a constant*/
+
+const product = 'Headset Gamer'; /* constant initialized & Value assigned */
+const stock = true; /* It have to be initialized . */
+/* JS A DINAMIC LANGUAGE */
+/* product = true;  You can't reassign the value of a constant */
+
+stock = true;
+        /*  you don't have to use ';' because it doesn't return an error and the sentence works.
+            'var 1stock ;' - You can't initialize a variable starting with a number.
+            'var -stock ;' - You can't initialize a variable starting with a special character.
+            'var stock-example ;' You can't initialize a variable with a middle dash but you cant with an underscore.
+            'var stock_example ;' You can initialize like this.
+        */ 
+const prod_1 = 'Laptop',
+    _stock = true,
+    category = 'Electronics';  /* to initialize many variables at the same time. */
+
+/* VARIABLE STYLES */
+const first_name = 'Edilberth';           /* Underscore */
+const secondName = 'Tulio';               /* Camelcase */
+const LastName = 'Aristizabal';           /* Pascal Case */
+const emailadress = 'edituari@wtf.com';   /* Lowercase */
