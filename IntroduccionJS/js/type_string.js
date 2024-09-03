82fc15241f39733d07b92cced9cf9fe7c002a3f6
dmdiegofer <diegofernandomunard@gmail.com>
2024-09-02 19:57:38 -0500

    Strings in JS. Initialize a string

---
+++ IntroduccionJS/js/type_string.js
@@ -0,0 +1,10 @@
+/* STRING */
+
+const product = "Monitor 20\" pulgadas"
+const product_ = 'Monitor 20' ;
+const product_2 = String("Monitor 30 pulgadas");
+const product_3 = new String("Monitor 40 pulgadas");
+
+console.log(typeof product);
+console.log(typeof product_2);
+console.log(typeof product_3);
