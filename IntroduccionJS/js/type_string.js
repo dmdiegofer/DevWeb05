39aa7f8a1ece78ced4bbf9ea84da95127c234176
dmdiegofer <diegofernandomunard@gmail.com>
2024-09-03 21:54:56 -0500

    Strings Methods
    some of the most used methods for strings

--- IntroduccionJS/js/type_string.js
+++ IntroduccionJS/js/type_string.js
@@ -8,3 +8,11 @@ const product_3 = new String("Monitor 40 pulgadas");
 console.log(typeof product);
 console.log(typeof product_2);
 console.log(typeof product_3);
+
+/* METHODS OF STRING */
+
+console.log(product.length); /* To show the lengh of a string */
+console.log(product.indexOf('Monitor')); /* to find a word a letter or something in a string 
+                            <0 doesn't exist in the string 
+                            >0 show the locations of the search with a count to left starting with cero */
+console.log(product.includes('Monitor')); /* Show us a boolean when find the search or not in a string */
