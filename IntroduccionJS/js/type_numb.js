e84bc846d601e99b9e175770fd27ce6038e9655e
dmdiegofer <diegofernandomunard@gmail.com>
2024-09-05 23:44:30 -0500

    Use of numbers in JS. Part I
    Just declare, some basic difference with other languages and operators.

---
+++ IntroduccionJS/js/type_numb.js
@@ -0,0 +1,23 @@
+/* USE OF NUMBERS IN JS */
+
+const numb_1 = 100;
+const numb_2 = 200;
+
+/* when you print in the browser's console a number, it will appear in purple. 
+    The strings instead (the variable or const with ¨¨) will be appear in black
+    its is a good tip to learn because you can identify data easy that way */
+
+const numb_decimal = 0.15;
+const numb_decimal2 = .20;
+const numb_negative = -50;
+
+/* In JavaScript the use of numbers is the same. Doesn´t matter if is a decimal or a negative and integer,
+   You don´t need a different type of constant or variable to declare one. */
+
+const sum = numb_1 + numb_2;
+const rest = numb_1 - numb_2;
+const multiply = numb_1 * numb_2;
+const divide = numb_1 / numb_2;
+const module = numb_1 % numb_2;
+/* The operatores are like most of the languages */
+console.log(numb_1);
