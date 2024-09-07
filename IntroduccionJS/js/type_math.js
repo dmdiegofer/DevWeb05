a91878b4e3760daa409a8fb4b97705bd71fdc6d2
dmdiegofer <69552519+dmdiegofer@users.noreply.github.com>
2024-09-06 23:05:10 -0500

    Numbers in JS: Math library. Part II
    Some of the functions of math library & JS syntax

---
+++ IntroduccionJS/js/type_math.js
@@ -0,0 +1,14 @@
+/* MATH LIBRARY BASIC FUNCTIONS */
+/* TIP -- IN BROWSER CONSOLE--> type window/intro and you will see all the resources available in js */
+
+let pi = Math.PI;
+let round = Math.round(2.5); /* Rounds to 2, if the variable is > than 2.6, rounded to 3 */ 
+let ceil = Math.ceil(2.1); /* Rounds to 3, always will round to ceil, means to the next integer */ 
+let floor = Math.floor(2.1); /* Rounds to 2, always will round to the floor, means to turn the decimal in cero it just lets the integer. */ 
+let sqrt = Math.sqrt(144); /* Square root. It's 12 */
+let abs = Math.abs(-200); /* It turns a numb into positive. */
+let min = Math.min(2,3,4,5,6,7,12,334); /* It finds the minor numb in a list. */
+let max = Math.max(2,3,4,5,6,7,12,334); /* It finds the maximum numb in a list. */
+let random = Math.random(); /* It a random numb between 0 and 1. */
+
+let random_ = Math.floor(Math.random * 30); /* It's a random integer (floor() it turns decimal into cero) multiple of 30 */
