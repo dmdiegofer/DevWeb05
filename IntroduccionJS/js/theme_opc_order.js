15a86b6060883f785cab73093e9a8b7919abc0e4
dmdiegofer <69552519+dmdiegofer@users.noreply.github.com>
2024-09-07 23:48:32 -0500

    Numbers in JS: Operations order & increments. Part III

---
+++ IntroduccionJS/js/theme_opc_order.js
@@ -0,0 +1,21 @@
+/* OPERATION ORDER 
+
+The order of math operations has the same rules than arithmetics
+*/
+let math = 20 + 60 * 2 ;
+
+/* The parentheses have priority as well */
+let math_ = (20 + 60) * 2 ;
+
+/* INCREMENTS */
+
+let points = 0 ;
+// plus one
+points ++ ;
+++ points ;
+// less one
+points -- ;
+-- points ;
+// plus and less N
+points += 4;
+points -= 6;
