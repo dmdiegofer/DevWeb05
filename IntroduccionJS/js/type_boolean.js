ef08f27aef7074d75f03253c9b9f8809c254bda4
dmdiegofer <69552519+dmdiegofer@users.noreply.github.com>
2024-09-10 21:58:29 -0500

    Boolean in JS

---
+++ IntroduccionJS/js/type_boolean.js
@@ -0,0 +1,9 @@
+/* BOOLEANS (TRUE OR FALSE FORMAT OR 1 AND 0 BINARY TYPE) */
+
+// IF YOU DONT USE QUOTES, IS A BOOLEAN, OTHERWISE IS A STRING
+const bool = true ;
+const bool_ = false ;
+// CLASIC JAVA INSTANCE // THE NEXT WAY TO INSTANT A BOOL IS INTERPRETED AS AN OBJECT NOT A COMMON VARIABLE
+let bool_inst = new Boolean(true) ;
+let bool_instance = new Bolean(false) ;
+// console.log(typeof bool_inst) ;
