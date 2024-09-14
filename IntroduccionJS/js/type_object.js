2b90a8051feb4373ed906c5b065389eef1c6da39
dmdiegofer <69552519+dmdiegofer@users.noreply.github.com>
2024-09-13 23:17:21 -0500

    Obj in JS. Destructuring. Part III
    HOW TO INSTANCE A VARIABLE/CONST WITH THE VALUE FROM AN OBJECT IN JS

--- IntroduccionJS/js/type_object.js
+++ IntroduccionJS/js/type_object.js
@@ -1,9 +1,8 @@
-/*  TYPE OBJECT */
-
+/*  TYPE OBJECT 
 const name_prod = 'Monitor 24"' ;
 var price = 300 ;
 let stock = true ;
-
+*/
 
 let product = {
     name_prod : 'Monitor 24"',
@@ -16,3 +15,18 @@ console.log(product) ; ------------> object with all it's attributes (name, pric
 console.log(product.price) ; ------> Price attribute only. The dot can be use to call object's methods of its own 
 console.log(product["stock"]) ; ---> Stock attribute only. This way to get the attribute of an object is just like the old Java way.
 */
+
+/* TO ADD NEW ATTRIBUTES TO AN OBJECT IN JS */
+product.image = 'image.jpg' ;
+
+/* TO DELETE AN ATTRIBUTES TO AN OBJECT IN JS */
+delete product.image ;
+
+/* HOW TO INSTANCE A VARIABLE/CONST WITH THE VALUE FROM AN OBJECT IN JS 
+let var_price = product.price ;
+var name = product.name_prod ;
+const stock_const = product.stock ;
+*/
+/* DESTRUCTURING ---> THE SAME PROCESS TO INSTANCE FROM AN OBJECT */
+const {price , name_prod , stock} = product ;
+// YOU CAN'T DESTRUCTURING AN ATTRIBUTE IF THERE ARE VARIABLES/CONSTANTS WITH THE SAME NAME
