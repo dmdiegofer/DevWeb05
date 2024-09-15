dac10f127efc6700a4846bfa3b3ed68746c33117
dmdiegofer <69552519+dmdiegofer@users.noreply.github.com>
2024-09-14 22:39:18 -0500

    Obj in JS. Some methods in Objs. Part IV

--- IntroduccionJS/js/type_object.js
+++ IntroduccionJS/js/type_object.js
@@ -1,15 +1,14 @@
+"use strict" ; // MEANS THAT THE CODE DOESN'T WORK IF FOUND ANY MISTAKE OR EXCEPTION WITHOUT VALIDATION
 /*  TYPE OBJECT 
 const name_prod = 'Monitor 24"' ;
 var price = 300 ;
 let stock = true ;
 */
-
 let product = {
     name_prod : 'Monitor 24"',
     price : 300 ,
     stock : true
 }
-
 /*  WAYS TO ACCESS
 console.log(product) ; ------------> object with all it's attributes (name, price, stock)
 console.log(product.price) ; ------> Price attribute only. The dot can be use to call object's methods of its own 
@@ -22,11 +21,14 @@ product.image = 'image.jpg' ;
 /* TO DELETE AN ATTRIBUTES TO AN OBJECT IN JS */
 delete product.image ;
 
-/* HOW TO INSTANCE A VARIABLE/CONST WITH THE VALUE FROM AN OBJECT IN JS 
+/* HOW TO INSTANCE A VARIABLE/CONST WITH THE VALUE FROM AN OBJECT IN JS */
 let var_price = product.price ;
 var name = product.name_prod ;
 const stock_const = product.stock ;
-*/
-/* DESTRUCTURING ---> THE SAME PROCESS TO INSTANCE FROM AN OBJECT */
+
+/* DESTRUCTURING ---> THE SAME PROCESS TO INSTANCE FROM AN OBJECT. WORKS WHEN IT HAS THE SAME NAME */
 const {price , name_prod , stock} = product ;
 // YOU CAN'T DESTRUCTURING AN ATTRIBUTE IF THERE ARE VARIABLES/CONSTANTS WITH THE SAME NAME
+
+/* METHODS WITH OBJ */
+Object.freeze(product) ; // IT AVOID THAN YOU CAN ADD OR DELETE ANY ATTRIBUTES TO AN OBJ
