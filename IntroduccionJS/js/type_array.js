a7516be235194fccdf422b622de865cdf4496033
dmdiegofer <69552519+dmdiegofer@users.noreply.github.com>
2024-09-16 22:15:42 -0500

    Arrays in JS. Syntax & Methods Part I

---
+++ IntroduccionJS/js/type_array.js
@@ -0,0 +1,13 @@
+/* ARRAYS IN JS*/
+
+let array_example = [1 , .3 , 4] ;
+const months = new Array['Enero','Febrero','Marzo'] ; 
+var example = ["String" , 10 , false , null , {price: 100 , stock: true} , [ 1 , 13 , 8 , 6 ] ] ;
+// console.table(array_example) ; //IN THE BROWSER CONSOLE LOOKS BETTER WITH CONSOLE.TABLE BECAUSE SHOWS THE POSITION AND THE VALUE IN A TABLE
+// console.log^(example[3]) ;
+
+// ARRAYS SOME METHODS
+months.length ;
+example.forEach( function(example) {
+    example ;
+})
